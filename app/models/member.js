b1200cba9f5cbef502be3067a501e9ed92331ce1
ans9611 <ans9611@gmail.com>
2021-08-10 12:11:04 -0400

    delete, update functionality finished

--- app/models/member.js
+++ app/models/member.js
@@ -3,15 +3,12 @@ const mongoose = require('mongoose')
 const memberSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true
   },
   balance: {
     type: String,
-    required: true
   },
   risk: {
     type: String,
-    required: true
   },
   owner: {
     type: mongoose.Schema.Types.ObjectId,
